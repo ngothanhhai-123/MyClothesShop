0ebf2d5419c567aee834f8c401ba7e784e401295
ngothanhhai-123 <ngothanhhai12032005@gmail.com>
2026-05-06 23:41:00 +0700

    update color

--- src/main/java/MyClothesShop/demo/entity/enums/Color.java
+++ src/main/java/MyClothesShop/demo/entity/enums/Color.java
@@ -1,5 +1,5 @@
 package MyClothesShop.demo.entity.enums; // Hoặc thư mục enums của bạn
 
 public enum Color {
-    BLACK, WHITE, RED, BLUE, LIGHT_BLUE, GRAY, YELLOW, GREEN
+    BLACK, WHITE, RED, BLUE, GRAY
 }
